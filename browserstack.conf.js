254d24b782fa21eaca4f2b50a0687704ee8f7a85
Leon Thorne <ldthorne96@gmail.com>
2018-08-09 15:12:53 -0400

    Add browsers to test on

--- browserstack.conf.js
+++ browserstack.conf.js
@@ -4,9 +4,89 @@ exports.config = {
   user: BROWSERSTACK_USERNAME,
   key: BROWSERSTACK_KEY,
 
-  capabilities: [{
-    browser: 'chrome'
-  }],
+  capabilities: [
+    {
+      'device': 'iPhone X',
+      'os_version': '11.0'
+    },
+    {
+      'device': 'iPhone 8',
+      'os_version': '11.0'
+    },
+    {
+      'device': 'Samsung Galaxy S9',
+      'os_version': '8.0'
+    },
+    {
+      'device': 'Google Pixel',
+      'os_version': '7.1'
+    },
+    {
+      'device': 'Samsung Galaxy S8',
+      'os_version': '7.0'
+    },
+    {
+      'device': 'Google Nexus 6',
+      'os_version': '6.0'
+    },
+    {
+      'os': 'Windows',
+      'os_version': '10',
+      'browser': 'Chrome',
+      'resolution': '1920x1080'
+    },
+    {
+      'os': 'Windows',
+      'os_version': '10',
+      'browser': 'Firefox',
+      'browser_version': '61.0',
+      'resolution': '1920x1080'
+    },
+    {
+      'os': 'Windows',
+      'os_version': '10',
+      'browser': 'Edge',
+      'browser_version': '17.0',
+      'resolution': '1920x1080'
+    },
+    {
+      'os': 'Windows',
+      'os_version': '10',
+      'browser': 'IE',
+      'browser_version': '11.0',
+      'resolution': '1920x1080'
+    },
+    {
+      'os': 'OS X',
+      'os_version': 'High Sierra',
+      'browser': 'Safari',
+      'browser_version': '11.0',
+      'resolution': '1920x1080'
+    },
+    {
+      'os': 'OS X',
+      'os_version': 'Sierra',
+      'browser': 'Safari',
+      'browser_version': '10.0',
+      'resolution': '1024x768'
+    },
+    {
+      'os': 'OS X',
+      'os_version': 'High Sierra',
+      'browser': 'Firefox',
+      'browser_version': '61.0',
+      'resolution': '1920x1080'
+    },
+    {
+      'os': 'OS X',
+      'os_version': 'High Sierra',
+      'browser': 'Chrome',
+      'browser_version': '68.0',
+      'resolution': '1920x1080'
+    }
+
+  ],
+  maxInstances: 10,
   specs: [
     './specs/happyPath.js'
   ],
